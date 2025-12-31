11b1e95ab006e832b2ab328672bfa1c80ee14c8b
Menul19 <menulmihisara2007@gmail.com>
2025-12-31 15:27:42 +0530

    Update main.dart

--- form_app/lib/main.dart
+++ form_app/lib/main.dart
@@ -1,35 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: SimpleCalculator(), debugShowCheckedModeBanner: false));
-
-class SimpleCalculator extends StatefulWidget {
-  @override
-  _SimpleCalculatorState createState() => _SimpleCalculatorState();
-}
-
-class _SimpleCalculatorState extends State<SimpleCalculator> {
-  String display = '0';
-
-  void onPressed(String text) {
-    setState(() {
-      if (text == 'AC') display = '0';
-      else display = text;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Simple Calculator')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(display, style: TextStyle(fontSize: 50)),
-            ElevatedButton(onPressed: () => onPressed('AC'), child: Text('Clear')),
-          ],
-        ),
-      ),
-    );
-  }
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(child: Text('Calculator App Success!', style: TextStyle(fontSize: 24))),
+    ),
+  ));
 }
