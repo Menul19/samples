eec1ebb94e68a10383e50200053616ba481f897d
Menul19 <menulmihisara2007@gmail.com>
2025-12-31 15:34:06 +0530

    Update main.dart

--- form_app/lib/main.dart
+++ form_app/lib/main.dart
@@ -1,9 +1,2 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: Center(child: Text('Calculator App Success!', style: TextStyle(fontSize: 24))),
-    ),
-  ));
-}
+void main() => runApp(MaterialApp(home: Scaffold(body: Center(child: Text('Success!')))));
